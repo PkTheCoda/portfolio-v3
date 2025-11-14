b394ce485ae575f4404ea12018a1d7247596703c
PkTheCoda <pranavkonjeti@gmail.com>
2025-11-14 15:23:39 -0600

    added bottom navbar + initial introduction content

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import figmaIcon from '../assets/figmasvg.svg'
+import uiuclogo from '../assets/uiuclogo.png'
 
 const Hero = () => {
   return (
-    <div className='bg-red-500 font-outfit'>
-      hi
+    <div className='font-fig py-4 tracking-tight flex flex-col gap-y-2 font-medium'>
+      <h3 className='two-title text-[#dd3e05]'>
+        Hey, I'm <span className="font-semibold">Pranav Konjeti!</span>
+      </h3>
+      <div className='three-title'>  
+        <div className='inline'>I'm an 18 y/o developer {' '}</div>
+          <div className="text-slate-600 inline">
+             building 
+            <FaGithub className='inline-block mx-2 text-black size-8 relative bottom-1'/>
+            user-facing software that emphasizes
+            <img src={figmaIcon} className='size-8 inline-block' alt="" />
+            beautiful UI/UX and design. I currently study {' '}
+            {/* <img src={uiuclogo} className='w-[18px] mx-2 inline-block relative bottom-0.5' alt="" /> */}
+            CS at UIUC. 
+          </div>
+      </div>
+      <h5 className='text-slate-400'>
+        TL;DR: I enjoy building cool sh*t that looks good
+      </h5>
     </div>
   )
 }
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import Dock from '../Components/Dock'
+import { FaHome, FaArchive, FaAccessibleIcon, FaRegArrowAltCircleDown } from "react-icons/fa";
+
+
 
 
 const Homepage = () => {
+
+  const DockItems = [
+    { icon: <FaHome size={18} />, label: 'Home', onClick: () => alert('Home!') },
+    { icon: <FaHome size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
+    { icon: <FaHome size={18} />, label: 'Profile', onClick: () => alert('Profile!') },
+    { icon: <FaHome size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
+  ];
+
   return (
     <>
-        <Hero />
+      <div className='min-h-screen bg-slate-100 flex items-center justify-center'>
+        <div className='max-w-[42rem] py-10 w-full mx-auto'>
+          <Hero />
+           <div className='py-10'>
+              <Dock 
+                items={DockItems}
+                panelHeight={68}
+                baseItemSize={50}
+                magnification={70}
+              />
+           </div>
+        </div>
+      </div>
     </>
   )
 }
